a45e48436c80a4950bb81c096cb32eb828540c22
Venkatesh Reddi <venkatehreddi@outlook.com>
2021-12-10 05:55:59 +0530

    Modified the code of inserting elements to array -- still doesn't work

--- Unit_03/Arrays/inserting_element_in_array.c
+++ Unit_03/Arrays/inserting_element_in_array.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 void main()
 {
-    int x;
-    int list[x + 1], num, pos;
-    printf("Enter the length of list: ");
-    scanf("%d", &x);
-    printf("Enter the elements for list: ");
-    for(int i = 0; i < x; i++) scanf("%d", &list[i]);
-    printf("Enter the number to be added followed by position where to be added: ");
-    scanf("%d %d", &num, &pos);
-    for(int i =  x; i >= pos; i--) list[i] = list[i - 1];
-    list[pos - 1] = num;
-    printf("List of elements after insertion of %d: ", num);
-    for(int i = 0; i < x + 1; i++) printf("%d\t", list[i]);
+    int N, y;
+    int num, pos, list[N];
+    printf("Enter the number of elements in the list: ");
+    scanf("%d", &y);
+    N = y + 1;
+    printf("Enter the elements: ");
+    for(int i = 0; i < N - 1; i++)
+    scanf("%d", &list[i]);
+    printf("Enter the number to be inserted in the list: ");
+    scanf("%d", &num);
+    printf("Enter the position of the above entered number: ");
+    scanf("%d", &pos);
+    for(int i = N; i > pos; i--)
+    list[i] = list[i - 1];
+    list[pos] = num;
+    for(int i = 0; i < N; i++)
+    printf("%d\t", list[i]);
 }
-
-/* input:
-    lenght of list = 5
-    elements of  list = 1 2 3 4 5
-    next prompt for num and at pos = 6 2
-
-    Expected output = 1 6 2 3 4 5
-
-    Actuall output = 1 2 3 4 5 5
-    
-    Try to fix this one... */
