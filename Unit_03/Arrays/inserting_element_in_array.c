ca53b0263f444c4e26e96f80b25123a20c24f92b
Venkatesh Reddi <venkatehreddi@outlook.com>
2021-12-12 11:21:52 +0530

    Completed the inserting element code finally

--- Unit_03/Arrays/inserting_element_in_array.c
+++ Unit_03/Arrays/inserting_element_in_array.c
@@ -1,23 +1,19 @@
-/* This programs adds a number to the array of numbers
-    User can specify the location of the new element and its value*/
 #include <stdio.h>
 void main()
 {
-    int N, y;  // defined the variabels which represent the available spaces in array
-    int num, pos, list[N]; // defined "num" variable which represents the new value to be added to list
-    printf("Enter the number of elements in the list: ");
-    scanf("%d", &y); // This is the temporary length of the array, which is further increased by 1
-    N = y + 1;
-    printf("Enter the elements: ");
-    for(int i = 0; i < N - 1; i++) // iterated to take all the values of the elements for the array
-    scanf("%d", &list[i]);
-    printf("Enter the number to be inserted in the list: ");
-    scanf("%d", &num); // This value is the number we want to insert in the array
-    printf("Enter the position of the above entered number: ");
-    scanf("%d", &pos); // The variable "pos" represents the position of the element in the array
-    for(int i = N; i > pos; i--) // Moving the elements in the list to make space for the new value
-    list[i] = list[i - 1]; 
-    list[pos] = num;  // inserting the new number to the array
-    for(int i = 0; i < N; i++) // Itterating over the array elements and printing all of them.
-    printf("%d\t", list[i]);
+    int array[100], position, n, c, value;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter the %d elements of the list: ", n);
+    for(c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+    printf("Enter the position for the new element in the array: ");
+    scanf("%d", &position);
+    printf("Enter the value of the new element: ");
+    scanf("%d", &value);
+    for(c = n - 1; c >= position - 1; c--)
+    array[c + 1] = array[c];
+    array[position - 1] = value;
+    for(c = 0; c < n + 1; c++)
+    printf("%d\t", array[c]);
 }
